cc49d244dddf404ee9b3c1c18797f4633f2147f0
Corvux <bryce.hadsell@gmail.com>
2018-09-28 15:27:35 +0000

    morning update 2

--- mybot.js
+++ mybot.js
@@ -1,22 +1,41 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const config = require("./config.json");
 
 client.on("ready", () => {
   console.log("I am ready!");
 });
 
-const prefix = "!";
+const prefix = config.prefix;
 
 client.on("message", (message) => {
   
   if (!message.content.startsWith(prefix) || message.author.bot) return;
   
-  if (message.content.toUpperCase().startsWith(prefix + "PING")) {
-    message.channel.send("pong!");
-  } else
-  if (message.content.toUpperCase().startsWith(prefix + "FOO")){
-    message.channel.send("bar");
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase()
+  
+  switch (command){
+    case 'ping' :
+      message.channel.send("pong");
+      break;
+      
+    case 'foo' :
+      message.channel.send("bar");
+      break;
+      
+    case 'asl' :
+      let [age, sex, location] = args;
+      
+      message.reply(`Hello ${message.author.username}, you are a ${age} year old ${sex} from ${location}`);
+      break;
+      
+      case 'say' :
+        let text = args.slice(1).join(" ");
+        message.delete();
+        message.channel.send(text);
+        break;
   }
 });
 
-client.login("NDk0NjQ3MzUzNjcyODU5NjU5.Do8FhA.LW83neF64K5M32wQhU2KUgq0Td4");
+client.login(config.token);
